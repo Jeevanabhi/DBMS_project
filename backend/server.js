83aecc581935c419c44537bbbde7a30905350802
Jeevanabhi <jeevanjenin@gmail.com>
2026-04-02 23:17:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use((req, res) => {
 });
 
 // Start the server immediately so Render detects an open port
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`=====================================`);
   console.log(`Server proudly running on port ${PORT}`);
   console.log(`=====================================`);
